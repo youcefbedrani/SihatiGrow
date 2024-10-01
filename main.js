9b35eb4f8520e228c043b6c25736ab575bef7121
youcefbedrani <youcefbedrani99@gmail.com>
2024-10-02 00:54:30 +0100

    The basic componetes with electron js

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Optional
+      contextIsolation: true, // Recommended for security
+      enableRemoteModule: false, // Disable remote module
+    },
+  });
+
+  win.loadFile('public/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi, Software engineering and welcome to your space</h1>
+      <hr />
+      <h3> Here we make our :</h3>
+      <ol>
+        <li>
+          {" "}
+          <i>Lab blood system</i>{" "}
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default App;
